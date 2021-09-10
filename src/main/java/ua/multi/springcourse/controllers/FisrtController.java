06713024bcc646d2bdfef4acb28dff2d43681a64
OsadtsivMykhailo <osadtsiv.m@gmail.com>
2021-09-10 11:30:56 +0300

    lesson 19 / Spring MVC. Параметры GET запроса. Аннотация @RequestParam.

--- src/main/java/ua/multi/springcourse/controllers/FisrtController.java
+++ src/main/java/ua/multi/springcourse/controllers/FisrtController.java
@@ -3,17 +3,27 @@ package ua.multi.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
-//@RequestMapping("/first")
+@RequestMapping("/first")
 public class FisrtController {
         @GetMapping("/hello")
-        public String helloPage() {
+        public String helloPage(HttpServletRequest request) {       //без параметрів кладе в параметри null
+            String name = request.getParameter("name");
+            String surname = request.getParameter("surname");
+
+            System.out.println("Hello, " + name + " " + surname);
             return "first/hello";
         }
 
         @GetMapping("/goodbye")
-        public String goodByePage() {
+        public String goodByePage(@RequestParam("name") String name,     //без параметрів не паше, 404
+                                  @RequestParam(value = "surname", required = false) String surname) { //без параметрів кладе в параметри null
+
+            System.out.println("Good bye, " + name + " " + surname);
             return "first/goodbye";
         }
 

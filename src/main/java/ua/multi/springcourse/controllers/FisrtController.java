17c82a29f5f4d7bb6a6ffdcbb31d78e559f06967
OsadtsivMykhailo <osadtsiv.m@gmail.com>
2021-09-10 12:00:28 +0300

    HW 20 / Spring MVC. calculator

--- src/main/java/ua/multi/springcourse/controllers/FisrtController.java
+++ src/main/java/ua/multi/springcourse/controllers/FisrtController.java
@@ -1,6 +1,7 @@
 package ua.multi.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,12 +20,45 @@ public class FisrtController {
             return "first/hello";
         }
 
-        @GetMapping("/goodbye")
-        public String goodByePage(@RequestParam("name") String name,     //без параметрів не паше, 404
-                                  @RequestParam(value = "surname", required = false) String surname) { //без параметрів кладе в параметри null
+//        @GetMapping("/goodbye")
+//        public String goodByePage(@RequestParam("name") String name,     //без параметрів не паше, 404
+//                                  @RequestParam(value = "surname", required = false) String surname) { //без параметрів кладе в параметри null
+//
+//            System.out.println("Good bye, " + name + " " + surname);
+//            return "first/goodbye";
+//        }
 
-            System.out.println("Good bye, " + name + " " + surname);
-            return "first/goodbye";
+        @GetMapping("/goodbye")
+         public String goodByePage(@RequestParam(value = "name", required = false) String name,
+                                   @RequestParam(value = "surname", required = false) String surname, Model model) {
+
+             //System.out.println("Good bye, " + name + " " + surname);
+            model.addAttribute("message", "Good bye, " + name + " " + surname);
+             return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a") int a,
+                             @RequestParam(value = "b") int b,
+                             @RequestParam(value = "action") String action, Model model) {
+
+        //System.out.println("Good bye, " + name + " " + surname);
+        if(action.equals("addition")) {
+            model.addAttribute("message", "Result is: " + (a + b));
         }
+
+        if(action.equals("substraction")) {
+            model.addAttribute("message", "Result is: " + (a - b));
+        }
+
+        if(action.equals("multiplication")) {
+            model.addAttribute("message", "Result is: " + (a * b));
+        }
+
+        if(action.equals("division")) {
+            model.addAttribute("message", "Result is: " + (a / b));
+        }
+        return "first/goodbye";
+    }
 
 }
